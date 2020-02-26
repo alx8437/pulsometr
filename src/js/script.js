bf2ab0a05c8418ee3bcd0c7a822d259d824489ac
Aleksandr <alx8437@mail.ru>
2020-02-26 15:44:15 +0300

    slide continion

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,23 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1300,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"></button>'
-      });
-  });
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.png"></button>',
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    arrows: false
+                }
+            },
+        ]
+    });
+});
