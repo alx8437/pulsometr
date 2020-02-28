2cb9573a3319fb4add473d17ca8ba637247decdb
Aleksandr_Zelenskiy <alx8437@mail.ru>
2020-02-28 07:00:00 +0300

    use tiny-slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+/* $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1300,
         adaptiveHeight: true,
@@ -20,4 +20,23 @@ $(document).ready(function() {
             },
         ]
     });
+}); */
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+
+  });
+
+document.querySelector('.prev').addEventListener('click', function() {
+    slider.goTo('prev');
 });
+
+document.querySelector('.next').addEventListener('click', function() {
+    slider.goTo('next');
+});
+
